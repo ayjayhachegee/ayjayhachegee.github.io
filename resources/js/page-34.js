23c30003e08c421ea01dbfa16bf055885eeec63f
ayjayhachegee <ajhg@tutanota.com>
2024-11-14 02:29:38 +0000

    Update code to fix QR code for mobile.

---
+++ resources/js/page-34.js
@@ -0,0 +1,13 @@
+function main() {
+    if (window.screen.width <= 1080) {
+        document.getElementById("qrCode").src = "https://media.discordapp.net/attachments/692931808916144181/1306444638605017088/qrCode2_v2.png?ex=6736b0e2&is=67355f62&hm=49b2dc49832a7fc305d387c5c31a2b1081f85a17675c0f298e7e5d8161864a34&=&quality=lossless";
+        document.getElementById("qrMessage").innerHTML = "See where it goes...";
+    }
+    if (window.location.href.indexOf("start") != -1) {
+        document.getElementById("qrMessageContainer").remove()
+    }
+}
+
+window.onload = function() {
+    main();
+};
