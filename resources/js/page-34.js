c1ec28a6c08632fd4c8995612c89763e7c7c0cf9
ayjayhachegee <ajhg@tutanota.com>
2024-11-17 19:58:23 +0000

    Updated the links.

--- resources/js/page-34.js
+++ resources/js/page-34.js
@@ -1,10 +1,17 @@
-function main() {
-    if (window.screen.width < 1081) {
-        document.getElementById("qrCode").src = "https://media.discordapp.net/attachments/692931808916144181/1306444638605017088/qrCode2_v2.png?ex=6736b0e2&is=67355f62&hm=49b2dc49832a7fc305d387c5c31a2b1081f85a17675c0f298e7e5d8161864a34&=&quality=lossless";
-        document.getElementById("qrMessage").innerHTML = "Now go back to desktop...";
+function enable_desktop_mode(replaceAll = false) {
+    if (replaceAll) {
+        document.getElementById('qrMessageContainer').classList.remove('is-hidden-touch');
+        document.getElementById('mobileDisplayContainer').classList.remove('is-hidden-desktop');
+        document.getElementById('mobileDisplayContainer').classList.add('is-hidden');
     }
+    document.getElementById("qrCode").src = "https://media.discordapp.net/attachments/692931808916144181/1306444638605017088/qrCode2_v2.png?ex=673b4e22&is=6739fca2&hm=97aa162b7153a196d5a0a03e84b85f54e2dd09fedefe74bada733e7452d18c9d&quality=lossless";
+    document.getElementById("qrMessage").innerHTML = "Now go back to desktop...";
+}
+
+function main() {
     if (window.location.href.indexOf("start") != -1) {
         document.getElementById("qrMessageContainer").remove()
+        document.getElementById("mobileDisplayContainer").remove()
     }
 }
 
