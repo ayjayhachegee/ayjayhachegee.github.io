75ef834478c87751dc9727d733508363685eab0e
ayjayhachegee <ajhg@tutanota.com>
2024-12-01 21:43:56 +0000

    Localised images.

--- resources/js/page-34.js
+++ resources/js/page-34.js
@@ -4,7 +4,7 @@ function enable_desktop_mode(replaceAll = false) {
         document.getElementById('mobileDisplayContainer').classList.remove('is-hidden-desktop');
         document.getElementById('mobileDisplayContainer').classList.add('is-hidden');
     }
-    document.getElementById("qrCode").src = "https://media.discordapp.net/attachments/692931808916144181/1306444638605017088/qrCode2_v2.png?ex=673b4e22&is=6739fca2&hm=97aa162b7153a196d5a0a03e84b85f54e2dd09fedefe74bada733e7452d18c9d&quality=lossless";
+    document.getElementById("qrCode").src = "../../../resources/media/images/qrCode2_v2.png";
     document.getElementById("qrMessage").innerHTML = "Now go back to desktop...";
 }
 
